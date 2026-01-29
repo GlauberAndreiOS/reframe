f01d9f35590aaaa171e39376f5c0eece820f0697
GlauberPain <andrei04@hotmail.com>
2026-01-29 11:27:04 -0400

    Refactor: Change AutomaticThought primary key to GUID
    - **Database**: Update `AutomaticThoughts` table schema to use `UUID` (Guid) for the `Id` column instead of `integer`.
    - **Migrations**: Add `AddAutomaticThoughtEntity` migration to apply the column type change and update `ApplicationDbContextModelSnapshot` to reflect the new model state.

--- Migrations/20260129152431_AddAutomaticThoughtEntity.Designer.cs
+++ Migrations/20260129152431_AddAutomaticThoughtEntity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using reframe.Data;
@@ -11,9 +12,11 @@ using reframe.Data;
 namespace reframe.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260129152431_AddAutomaticThoughtEntity")]
+    partial class AddAutomaticThoughtEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
